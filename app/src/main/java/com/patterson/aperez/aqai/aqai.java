f3d442c9899bcad2c3fe6f1655664ff6451f8a43
D1rtyD3mon <tsspsycho504@gmail.com>
2018-10-16 09:39:48 -0400

    Copied all the files to anew folder

---
+++ app/src/main/java/com/patterson/aperez/aqai/aqai.java
@@ -0,0 +1,25 @@
+package com.patterson.aperez.aqai;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class aqai extends AppCompatActivity {
+    TextView txt;
+    Button btn;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_aqai);
+        txt= (TextView) findViewById(R.id.truAns);
+        btn= (Button) findViewById(R.id.tru);
+
+        btn.setOnClickListener(new
+        }
+
+
+
+    }
+}
